1bbbd7f317c8a53f9a6ec567ee16f468ab0f34e1
MarkMutt <lleasy8@gmail.com>
2025-08-11 15:39:57 -0400

    added db migrations

--- app/services/auth/auth.server.ts
+++ app/services/auth/auth.server.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/db.server";
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
   socialProviders: {
@@ -11,5 +12,7 @@ export const auth = betterAuth({
   },
   database: drizzleAdapter(db, {
     provider: "pg",
+    usePlural: true,
   }),
+  plugins: [admin()],
 });
