9cb31b7d7bbb461c8a5fe646e675aee19aa46698
MarkMutt <lleasy8@gmail.com>
2025-08-19 12:47:47 -0400

    add applications schema

--- app/services/db/schema/food_pantry.ts
+++ app/services/db/schema/food_pantry.ts
@@ -7,7 +7,10 @@ import {
   serial,
   pgSchema,
   pgEnum,
+  uuid,
 } from "drizzle-orm/pg-core";
+import { users } from "./auth_schema";
+import { randomUUID } from "crypto";
 
 export const foodPantry = pgSchema("food_pantry");
 
@@ -23,13 +26,20 @@ export const eventStatusEnum = pgEnum("event_status", [
   "archived",
 ]);
 
+export const applicationStatusEnum = pgEnum("application_status", [
+  "submitted",
+  "accepted",
+  "waitlist",
+  "declined",
+]);
+
 const timestamps = {
   createdAt: timestamp("created_at").$defaultFn(
     () => /* @__PURE__ */ new Date()
   ),
-  updatedAt: timestamp("updated_at").$defaultFn(
-    () => /* @__PURE__ */ new Date()
-  ),
+  updatedAt: timestamp("updated_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .$onUpdateFn(() => new Date()),
 };
 
 export const programs = foodPantry.table("programs", {
@@ -47,5 +57,42 @@ export const events = foodPantry.table("events", {
   eventDate: timestamp("event_date"),
   openDate: timestamp("open_date"),
   closeDate: timestamp("close_date"),
+  ...timestamps,
+});
+
+export const students = foodPantry.table("students", {
+  id: uuid().primaryKey().defaultRandom(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  school: text().notNull(),
+  userId: text("user_id").references(() => users.id),
+  ...timestamps,
+});
+
+export const profiles = foodPantry.table("profiles", {
+  id: text().primaryKey(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  street: text().notNull(),
+  street2: text(),
+  city: text().notNull(),
+  state: text().notNull(),
+  zip: text().notNull(),
+});
+
+export const applications = foodPantry.table("applications", {
+  id: serial().primaryKey(),
+  userId: text("user_id").references(() => users.id),
+  programID: integer("program_id").references(() => programs.id),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  street: text().notNull(),
+  street2: text(),
+  city: text().notNull(),
+  state: text().notNull(),
+  zip: text().notNull(),
+  email: text().notNull(),
+  status: applicationStatusEnum().default("submitted"),
+  acceptedDate: timestamp("accepted_date"),
   ...timestamps,
 });
